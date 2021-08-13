c8ff3d361a18f66c98cb349b2bb455888f3e9f93
tyler <taehyun9202@hotmail.com>
2021-08-13 18:22:16 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import "../configureAmplify";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <nav className="w-full py-4 px-12 border-b border-gray-300">
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/profile">
+          <a className="ml-4">Profile</a>
+        </Link>
+        <Link href="/create-post">
+          <a className="ml-4">Create Post</a>
+        </Link>
+      </nav>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
